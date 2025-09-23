7c0f72bbaecc6b2afa6ff7ddafe5e5ade9555afe
gregoryreis <greis003@fiu.edu>
2025-09-22 20:16:42 -0400

    adding week 5 of classes

--- flaskWebApp1.py
+++ flaskWebApp1.py
@@ -1,14 +1,2 @@
-from flask import Flask, request
+from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "<h1>Welcome to Internship Ready</h1>"
-
-@app.route('/aboutUs/<string:name>')
-def aboutUs(name):
-    return f"<br> <h3> My name is {name} and I am enrolled in CIS 3950.</h3>"
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5050)
