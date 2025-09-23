a37a5ae9fc5f50c5d47a9fbdbfb2f07ce4718ef6
gregoryreis <greis003@fiu.edu>
2025-09-22 20:10:52 -0400

    adding week 5 of classes

---
+++ apiLecture.py
@@ -0,0 +1,23 @@
+"""
+You will need to visit https://api.nasa.gov/ and get a free API key
+You will need to create the files:
+    1) .env
+    2) .gitignore
+You will need to install:
+    1) pip install python-dotenv
+    2) pip install requests
+    To use your environment variable:
+    from dotenv import load_dotenv
+    load_dotenv()
+    os.getenv("SECRET_KEY")
+"""
+
+import requests
+import main_functions
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# NASA's API url
+url = "https://api.nasa.gov/planetary/apod?api_key="
+
